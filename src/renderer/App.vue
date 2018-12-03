6eb4400636e98613dfe69e57184b0441ff470f8f
Angel Castro <al.castro@gmail.com>
2018-12-03 16:39:30 +0100

    CSS now loaded properly.

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -16,5 +16,14 @@
 </script>
 
 <style>
-  /* CSS */
+  /* Bootstrap */
+  @import url('~@/assets/css/bootstrap/css/bootstrap.min.css');
+  /* Custom fonts for this template */
+  @import url('~@/assets/css/fontawesome-free/css/all.min.css');
+  /* Page level plugin CSS */
+  @import url('~@/assets/css/datatables/datatables.bootstrap4.min.css');
+  /* Custom styles for this template */
+  @import url('~@/assets/css/sb-admin.css');
+  @import url('~@/assets/css/nunito/nunito.css');
+  @import url('~@/assets/css/animate/animate.min.css');
 </style>
