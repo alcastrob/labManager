08da501cddb68d0c10b11f1e685a762d4d789215
Angel Castro <al.castro@gmail.com>
2019-01-18 12:52:54 +0100

    Multipage invoices working.

--- src/renderer/components/Labels/Invoice.vue
+++ src/renderer/components/Labels/Invoice.vue
@@ -9,7 +9,9 @@ import {Printd} from 'printd'
 var path = require('path')
 var fs = require('fs')
 import invoicePage from './invoicePage'
-import { getInvoice, getWorkIndications } from '../../../main/dal.js'
+import { getConfigValues, getInvoice, getWorkIndications } from '../../../main/dal.js'
+
+const MAX_NUMBER_OF_LINES_PER_PAGE = 35
 
 export default {
   name: 'invoice',
@@ -25,7 +27,12 @@ export default {
         style: 'currency',
         currency: 'EUR'
       }),
-      cssText: ''
+      cssText: '',
+      logo: '',
+      vatNumber: '',
+      footer: '',
+      pageLogosLoaded: {},
+      instances: []
     }
   },
   props: {
@@ -35,43 +42,106 @@ export default {
     }
   },
   methods: {
-    logoLoaded() {
-      this.imgLoaded = true
-    },
-    print (parentContainer) {
-      var ComponentClass = Vue.extend(invoicePage)
-      var instance = new ComponentClass({
-        propsData: {
-          invoice: this.invoice,
-          works: this.invoiceWorks,
-          indications: this.indications,
-          pageNumber: 1
-        }
-      })
-      var instance2 = new ComponentClass({
-        propsData: {
-          invoice: this.invoice,
-          works: this.invoiceWorks,
-          indications: this.indications,
-          pageNumber: 2
-        }
-      })
-
-      instance.$mount()
-      instance2.$mount()
-      this.$refs.container.appendChild(instance.$el)
-      this.$refs.container.appendChild(instance2.$el)
-      const d = new Printd()
-      d.print(this.$el, this.cssText)
-    },
-    loadData: async function() {
+    print: async function () {
       var x = await getInvoice(this.invoiceId)
       this.invoice = x.invoice
       this.invoiceWorks = x.invoiceWorks
       for (var work of this.invoiceWorks){
         this.indications[work.IdTrabajo] = await getWorkIndications(work.IdTrabajo)
       }
-    }
+
+      this.pageLogosLoaded = {}
+      var ComponentClass = Vue.extend(invoicePage)
+      this.instances = []
+      var currentPageLines = 0
+      var currentPage = 1
+
+      var worksToPrint = []
+      var indicationsToPrint = []
+
+      for (var currentWork of this.invoiceWorks){
+        var toAdd = 2 + this.indications[currentWork.IdTrabajo].length
+        if (currentPageLines + toAdd <= MAX_NUMBER_OF_LINES_PER_PAGE) {
+          worksToPrint.push(currentWork)
+          indicationsToPrint[currentWork.IdTrabajo]=this.indications[currentWork.IdTrabajo]
+          currentPageLines += toAdd
+        }
+        else {
+          this.pageLogosLoaded[currentPage] = false
+          var instance = new ComponentClass({
+            propsData: {
+              invoice: this.invoice,
+              //The array must be cloned for this specific page
+              works: worksToPrint.slice(0),
+              indications: indicationsToPrint.slice(0),
+              isFirstPage: currentPage === 1,
+              isLastPage: false,
+              pageNumber: currentPage++,
+              cssText: this.cssText,
+              logo: this.logo,
+              vatNumber: this.vatNumber,
+              footer: this.footer
+            }
+          })
+          this.instances.push(instance)
+          instance.$mount()
+          instance.waitLogo(this.waitLogoCallback)
+          this.$refs.container.appendChild(instance.$el)
+          // Now we reset the arrays and include the current work in the next page
+          worksToPrint = [currentWork]
+          indicationsToPrint = []
+          indicationsToPrint[currentWork.IdTrabajo] = this.indications[currentWork.IdTrabajo]
+          currentPageLines = 2 + this.indications[currentWork.IdTrabajo].length
+        }
+      }
+
+      if (indicationsToPrint.length !== 0){
+        //We have something to print on the last page
+        this.pageLogosLoaded[currentPage] = false
+        var instance = new ComponentClass({
+            propsData: {
+              invoice: this.invoice,
+              //The array must be cloned for this specific page
+              works: worksToPrint.slice(0),
+              indications: indicationsToPrint.slice(0),
+              isFirstPage: currentPage === 1,
+              isLastPage: true,
+              pageNumber: currentPage++,
+              cssText: this.cssText,
+              logo: this.logo,
+              vatNumber: this.vatNumber,
+              footer: this.footer
+            }
+          })
+          this.instances.push(instance)
+          instance.$mount()
+          instance.waitLogo(this.waitLogoCallback)
+          this.$refs.container.appendChild(instance.$el)
+      }
+      this.realPrint()
+    },
+    waitLogoCallback(pageNumber) {
+      this.pageLogosLoaded[pageNumber] = true
+    },
+    realPrint() {
+      if (! _.every(Object.values(this.pageLogosLoaded), (val) => {return val})) {
+        window.setTimeout(this.realPrint, 500)
+        return
+      } else {
+        const d = new Printd()
+        d.print(this.$el, this.cssText)
+
+        for (var currentInstance of this.instances) {
+          this.$refs.container.removeChild(currentInstance.$el)
+        }
+      }
+    },
+    loadData: async function() {
+      var config = await getConfigValues(['logo', 'vatNumber', 'invoiceFooter'])
+      this.logo = 'data:image/png;base64,' + _.find(config, ['clave', 'logo']).valor
+      this.vatNumber = _.find(config, ['clave', 'vatNumber']).valor
+      this.footer = _.find(config, ['clave', 'invoiceFooter']).valor
+    },
   },
   created () {
     this.loadData()
--- src/renderer/components/Labels/InvoicePage.vue
+++ src/renderer/components/Labels/InvoicePage.vue
@@ -7,7 +7,7 @@
       CIF: {{vatNumber}}
     </div> <!-- col-sm-6 -->
     <div class="col-sm-6">
-      <h1 class="text-uppercase text-right">Factura</h1>
+      <h1 class="text-uppercase text-right mt-3">Factura</h1>
     </div> <!-- col-sm-6 -->
   </div> <!-- row -->
   <div class="row mt-2">
@@ -27,79 +27,70 @@
     </div> <!-- col-sm-6 -->
   </div> <!-- row -->
   <div class="row mt-5">
-    <table class="table table-invoice" width="100%" cellspacing="0">
-      <thead>
-        <tr>
-          <th class="text-left" style="width: 60%;">Concepto</th>
-          <th class="text-right" style="width: 10%;">Cantidad</th>
-          <th class="text-right" style="width: 10%;">Dto.</th>
-          <th class="text-right" style="width: 10%;">P. Unidad</th>
-          <th class="text-right" style="width: 10%;">Subtotal</th>
-        </tr>
-      </thead>
-      <tbody>
-        <template v-for="work in works">
-          <tr v-bind:key="work.IdTrabajo" class="pagebreak">
-            <td class="text-left">
-              <span class="font-weight-bold">Nº Trabajo: {{work.IdTrabajo}}. Fecha: {{format(work.FechaEntrada)}}</span><br>
-              <span class="">{{work.Paciente}}</span>
-            </td>
-            <td class="text-right">1</td>
-            <td class="text-right">-</td>
-            <td class="text-right">{{moneyFormatter.format(work.PrecioFinal)}}</td>
-            <td class="text-right">{{moneyFormatter.format(work.PrecioFinal)}}</td>
+    <div class="col-sm-12" style="height: 1075px;">
+      <div v-if="!isFirstPage" class="text-right font-italic">       ...continúa desde la página anterior.
+      </div>
+      <table class="table table-invoice" width="100%" cellspacing="0">
+        <thead>
+          <tr>
+            <th class="text-left" style="width: 60%;">Concepto</th>
+            <th class="text-right" style="width: 10%;">Cantidad</th>
+            <th class="text-right" style="width: 10%;">Dto.</th>
+            <th class="text-right" style="width: 10%;">P. Unidad</th>
+            <th class="text-right" style="width: 10%;">Subtotal</th>
           </tr>
-          <tr class="leapTr dontBreakHere" v-for="indication in indications[work.IdTrabajo]" v-bind:key="indication.IdTrabajoDetalle">
-            <td class="text-left">
-              <span class="text-monospace">{{indication.Descripcion}}</span>
-            </td>
-            <td class="text-right"></td>
-            <td class="text-right"></td>
-            <td class="text-right"></td>
-            <td class="text-right"></td>
-          </tr>
-          <tr class="leapTr dontBreakHere" v-for="indication in indications[work.IdTrabajo]" v-bind:key="indication.IdTrabajoDetalle + 'b'">
-            <td class="text-left">
-              <span class="text-monospace">{{indication.Descripcion}}</span>
-            </td>
-            <td class="text-right"></td>
-            <td class="text-right"></td>
-            <td class="text-right"></td>
-            <td class="text-right"></td>
-          </tr>
-        </template>
-      </tbody>
-    </table>
-    <footer>
-      <div class="text-right printed-1 pb-2">Página {{pageNumber}}</div>
-      <span>{{footer}}</span>
-    </footer>
+        </thead>
+        <tbody>
+          <template v-for="work in works">
+            <tr v-bind:key="work.IdTrabajo" class="pagebreak">
+              <td class="text-left">
+                <span class="font-weight-bold">Nº Trabajo: {{work.IdTrabajo}}. Fecha: {{format(work.FechaEntrada)}}</span><br>
+                <span class="">{{work.Paciente}}</span>
+              </td>
+              <td class="text-right">1</td>
+              <td class="text-right">-</td>
+              <td class="text-right">{{moneyFormatter.format(work.PrecioFinal)}}</td>
+              <td class="text-right">{{moneyFormatter.format(work.PrecioFinal)}}</td>
+            </tr>
+            <tr class="leapTr dontBreakHere" v-for="indication in indications[work.IdTrabajo]" v-bind:key="indication.IdTrabajoDetalle">
+              <td class="text-left">
+                <span class="text-monospace">{{indication.Descripcion}}</span>
+              </td>
+              <td class="text-right"></td>
+              <td class="text-right"></td>
+              <td class="text-right"></td>
+              <td class="text-right"></td>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+      <div class="text-right font-weight-bold" v-if="isLastPage">Total: {{moneyFormatter.format(invoice.Total)}}</div>
+      <div class="text-right font-italic" v-else>...suma y sigue.</div>
+    </div> <!-- col-sm-12 -->
+  </div> <!-- row -->
+  <div class="text-right">Página {{pageNumber}}</div>
+  <div class="fixed-bottom printed-1">
+    <div class="text-justify printed">{{footer}}</div>
   </div>
+  <!-- <footer>
+  </footer> -->
 </div>
 </template>
 
 <script>
-import _ from 'lodash'
 import moment from 'moment'
-import { Printd } from 'printd'
-var path = require('path')
-var fs = require('fs')
-import { getConfigValues, getInvoice, getWorkIndications } from '../../../main/dal.js'
 
 export default {
   name: 'invoicePage',
   data () {
     return {
-      cssText: '',
       imgLoaded: false,
-      logo: '',
-      vatNumber: '',
-      footer: '',
       parentContainer: null,
       moneyFormatter: new Intl.NumberFormat('es-ES', {
         style: 'currency',
         currency: 'EUR'
-      })
+      }),
+      parentCallback: undefined
     }
   },
   props: {
@@ -118,43 +109,51 @@ export default {
     pageNumber: {
       type: Number,
       required: true
+    },
+    isFirstPage: {
+      type: Boolean,
+      required: true
+    },
+    isLastPage: {
+      type: Boolean,
+      required: true
+    },
+    cssText: {
+      type: String,
+      required: true
+    },
+    logo: {
+      type: String,
+      required: true
+    },
+    vatNumber: {
+      type: String,
+      required: true
+    },
+    footer: {
+      type: String,
+      required: true
     }
   },
   methods: {
     logoLoaded() {
       this.imgLoaded = true
     },
-    print (container) {
-      this.parentContainer = container
-      this.parentContainer.appendChild(this.$el)
-      this.waitLogoAndPrint()
-    },
-    waitLogoAndPrint(){
+    waitLogo(callback){
+      if (callback !== undefined) {
+        this.parentCallback = callback
+      }
       if (!this.imgLoaded) {
-        window.setTimeout(this.waitLogoAndPrint, 1000)
+        window.setTimeout(this.waitLogo, 500)
         return
       } else {
-        this.$forceUpdate()
-
-        const d = new Printd()
-        d.print(this.$el, this.cssText)
-        //this.parentContainer.removeChild(this.$el)
+        //this.$forceUpdate()
+        this.parentCallback(this.pageNumber)
       }
     },
     format(date) {
       return moment(date).format('DD/MM/YYYY')
-    },
-    loadData: async function() {
-      var config = await getConfigValues(['logo', 'vatNumber', 'invoiceFooter'])
-      this.logo = 'data:image/png;base64,' + _.find(config, ['clave', 'logo']).valor
-      this.vatNumber = _.find(config, ['clave', 'vatNumber']).valor
-      this.footer = _.find(config, ['clave', 'invoiceFooter']).valor
     }
-  },
-  created () {
-    this.loadData()
-    this.cssText = fs.readFileSync(path.resolve(__static, 'printed.css'), 'UTF-8')
-    this.cssText += fs.readFileSync(path.resolve(__static, 'bootstrap.min.css'), 'UTF-8')
   }
 }
 </script>
