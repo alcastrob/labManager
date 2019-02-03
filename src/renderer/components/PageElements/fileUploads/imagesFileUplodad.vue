d975e73f62b625c5294128cfa5348648fc67e262
Angel Castro <al.castro@gmail.com>
2019-02-03 21:53:33 +0100

    Continue working on fullscreen carousel

--- src/renderer/components/PageElements/fileUploads/imagesFileUplodad.vue
+++ src/renderer/components/PageElements/fileUploads/imagesFileUplodad.vue
@@ -4,20 +4,61 @@
       <fileUploadBase class="col-md-12" type="image/*" :multipleFiles="true" ref="fileUpload"></fileUploadBase>
     </div> <!-- row -->
     <div class="row text-center text-lg-left">
-      <div class="col-md-4 pb-3 text-center" v-for="image in value" v-bind:key="hash(image) + '-' + new Date().getTime()">
-        <a href="#" class="mb-4">
+      <div class="col-md-4 pb-3 text-center" v-for="image in value" v-bind:key="hash(image) + '-a-' + new Date().getTime()">
+        <a href="#" class="mb-4" @click="viewImage(image)">
           <img class="img-fluid img-thumbnail thumbnail" :src="image" title="Haga click para visualizar">
         </a>
         <button class="btn btn-secondary btn-sm" title="Descargar la imagen al ordenador" @click="downloadImage(image)"><i class="fas fa-download"></i></button>
         <button class="btn btn-secondary btn-sm" title="Borrar la imagen" @click="deleteImage(image)"><i class="fas fa-times-circle"></i></button>
       </div> <!-- col-md-4 -->
     </div> <!-- row -->
-  </div> <!-- container -->
+    <div id="slider" ref="slider" class="carousel slide overlay" :class="{'displayNone': !fullscreen}" data-ride="carousel">
+      <ol class="carousel-indicators" ref="imageIndicators">
+        <li v-for="(image, idx) in value" v-bind:key="hash(image) + '-b-' + new Date().getTime()" data-target="#slider" :data-slice-to="value.indexOf(image)" :class="{ active: idx === 0 }"></li>
+      </ol>
+      <div class="carousel-inner" ref="carouselImages">
+        <div class="carousel-item" v-for="(image, idx) in value" v-bind:key="hash(image) + '-c-' + new Date().getTime()">
+          <img class="d-block img-fluid" :src="image">
+        </div>
+      </div>
+      <a href="#slider" class="carousel-control-prev" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a href="#slider" class="carousel-control-next" data-slide="next">
+          <span class="carousel-control-next-icon"></span>
+        </a>
+    </div>
+  </div>
+
 </template>
 
 <script>
 import fileUploadBase from './fileUploadBase'
+import Vue from 'vue'
 const dialog = require('electron').remote.dialog
+
+document.onkeydown = e => {
+  if (e === undefined) {
+    bus.$emit('esc')
+  } else {
+    switch (e.keyCode) {
+      case 27:
+        bus.$emit('esc')
+        break
+      case 37:
+        // bus.$emit('goPrev')
+        break
+      case 39:
+        // bus.$emit('goNext')
+        break
+      default:
+        console.log(e)
+        break
+    }
+  }
+}
+
+const bus = new Vue()
 
 export default {
   name: 'imagesFileUpload',
@@ -25,7 +66,9 @@ export default {
     fileUploadBase
   },
   data () {
-    return {}
+    return {
+      fullscreen: false
+    }
   },
   props: {
     value: {
@@ -62,15 +105,34 @@ export default {
     },
     upload(files){
       for (var file of files){
-        this.viewFile(file)
+        this.viewThubnails(file)
       }
     },
-    viewFile(file){
+    viewImage(image) {
+      this.fullscreen = true
+      var position = this.value.indexOf(image)
+      
+      // for (var i = 0; i < this.value.length; i++){
+      //   if (i === position) {
+      //     this.$refs.carouselImages.childNodes[i].classList.add('active')
+      //     // this.$refs.imageIndicators.childNodes[i].classList.add('active')
+      //     console.log('active ' + i)
+      //   } else {
+      //     this.$refs.carouselImages.childNodes[i].classList.remove('active')
+      //     // this.$refs.imageIndicators.childNodes[i].classList.remove('active')
+      //     console.log('inactive ' + i)
+      //   }
+      // }
+    },
+    exitFullscreen() {
+      this.fullscreen = false
+    },
+    viewThubnails(file){
       var reader = new FileReader()
-      reader.onloadend = this.viewFile_sidecar
+      reader.onloadend = this.viewThubnails_sidecar
       reader.readAsDataURL(file)
     },
-    viewFile_sidecar(e){
+    viewThubnails_sidecar(e){
       this.value.push(e.currentTarget.result)
     },
     hash: function(data) {
@@ -83,6 +145,12 @@ export default {
       }
       return hash
     }
+  },
+  created() {
+    bus
+      .$on('goPrev', this.prev)
+      .$on('goNext', this.next)
+      .$on('esc', this.exitFullscreen)
   }
 }
 </script>
@@ -92,4 +160,14 @@ img.thumbnail {
   max-width: 100px;
   max-height: 200px;
 }
+
+.overlay {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  background: rgba(51,51,51,0.7);
+  z-index: 100;
+  }
 </style>
